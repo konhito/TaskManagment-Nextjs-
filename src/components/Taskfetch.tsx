3950ad8ae81d42a85464e35c2e5b66d1e8eee7c2
konhito <konhito02@gmail.com>
2025-02-05 14:59:59 +0530

    adding local storange for smoother operation

--- src/components/Taskfetch.tsx
+++ src/components/Taskfetch.tsx
@@ -5,10 +5,11 @@ export default async function GetTask() {
   try {
     const response = await axios.get(`${Backend_url}/api/tasks`);
     const task = response.data;
+    localStorage.setItem("task", task);
     console.log(task);
     return task;
   } catch (error) {
     console.error("Error while fetching the data:", error);
-    return null; 
+    return null;
   }
 }
